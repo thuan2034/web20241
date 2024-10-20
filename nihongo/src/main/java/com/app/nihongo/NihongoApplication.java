a3b7bf225a62571c9c94cbad408a9b1c98ec88d3
ezzio103 <tuancho1003hn@gmail.com>
2024-10-20 21:04:27 +0700

    entity

---
+++ nihongo/src/main/java/com/app/nihongo/NihongoApplication.java
@@ -0,0 +1,13 @@
+package com.app.nihongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NihongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NihongoApplication.class, args);
+	}
+
+}
