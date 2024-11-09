4367bb3a44ec67c3a11ac7d04a65deabc3d38e77
4l0ne4ever <thuyetdoras.3@gmail.com>
2024-11-09 10:50:51 +0700

    ui lesson

--- nihongofe/app/lesson/page.tsx
+++ nihongofe/app/lesson/page.tsx
@@ -5,24 +5,56 @@ const getLesson = async () => {
   return {
     id: 1,
     challenges: [
-      { challengeOptions: ["option1", "option2"], completed: false },
-      { challengeOptions: ["option3", "option4"], completed: false },
-      { challengeOptions: ["option5", "option6"], completed: false },
+      {
+        challengeOptions: [
+          { id: 1, option: "Correct Answer 1", isCorrect: true },
+          { id: 2, option: "Incorrect Answer 1", isCorrect: false },
+          { id: 3, option: "Incorrect Answer 2", isCorrect: false },
+          { id: 4, option: "Incorrect Answer 3", isCorrect: false },
+        ],
+        completed: false,
+        type: "SELECT",
+        question: "What is the correct answer for question 1?",
+      },
+      {
+        challengeOptions: [
+          { id: 1, option: "Correct Answer 2", isCorrect: true },
+          { id: 2, option: "Incorrect Answer 1", isCorrect: false },
+          { id: 3, option: "Incorrect Answer 2", isCorrect: false },
+          { id: 4, option: "Incorrect Answer 3", isCorrect: false },
+        ],
+        completed: false,
+        type: "SELECT",
+        question: "What is the correct answer for question 2?",
+      },
+      {
+        challengeOptions: [
+          { id: 1, option: "Correct Answer 3", isCorrect: true },
+          { id: 2, option: "Incorrect Answer 1", isCorrect: false },
+          { id: 3, option: "Incorrect Answer 2", isCorrect: false },
+          { id: 4, option: "Incorrect Answer 3", isCorrect: false },
+        ],
+        completed: false,
+        type: "SELECT",
+        question: "What is the correct answer for question 3?",
+      },
     ],
     hearts: 5,
   };
 };
 
 const LessonPage = async () => {
-  const lessonData = getLesson();
-  const [lesson] = await Promise.all([lessonData]);
+  const lesson = await getLesson();
+
   if (!lesson) {
     redirect("/learn");
   }
+
   const initialPercentage =
     (lesson.challenges.filter((challenge) => challenge.completed).length /
       lesson.challenges.length) *
     100;
+
   return (
     <Quiz
       initialLessonId={lesson.id}
