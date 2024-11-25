00d676c36c7902d5e99e3093adb556115df0aaa2
Thuan Nguyen <thuannguyen2034@gmail.com>
2024-11-25 09:41:01 +0700

    data for japanese character

---
+++ nihongofe/app/(main)/character/charactersData.ts
@@ -0,0 +1,15 @@
+export const hiragana = [
+    { character: "あ", romanji: "a" },
+    { character: "い", romanji: "i" },
+  ];
+  
+  export const katakana = [
+    { character: "ア", romanji: "a" },
+    { character: "イ", romanji: "i" },
+  ];
+    
+    export const kanji = [
+      { character: "日", romanji: "nichi" },
+      { character: "本", romanji: "hon" },
+    ];
+    
