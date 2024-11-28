90950e33965fb9e77eb7868ee71b81360d5732b8
Ezzio103 <tuancho1003hn@gmail.com>
2024-11-28 22:05:28 +0700

    question

--- nihongo/src/main/java/com/app/nihongo/service/question/QuestionService.java
+++ nihongo/src/main/java/com/app/nihongo/service/question/QuestionService.java
@@ -1,8 +1,15 @@
 package com.app.nihongo.service.question;
 
+import com.app.nihongo.dto.UserFailedQuestionDTO;
 import com.app.nihongo.enums.QuestionType;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface QuestionService {
     ResponseEntity<?> getQuestionsByTypeAndLessonId(Integer userId, QuestionType type, Integer lessonId);
+
+    List<UserFailedQuestionDTO> getFailedQuestionsByUserId(Integer userId);
+
+    ResponseEntity<?> getQuestionContentByTypeAndId(QuestionType type, Integer questionId);
 }
