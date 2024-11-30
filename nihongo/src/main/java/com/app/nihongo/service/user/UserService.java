33e6b7b7224a8f081bfcccf93ec49224098f6ff2
Ezzio103 <tuancho1003hn@gmail.com>
2024-11-30 23:44:41 +0700

    user

--- nihongo/src/main/java/com/app/nihongo/service/user/UserService.java
+++ nihongo/src/main/java/com/app/nihongo/service/user/UserService.java
@@ -1,11 +1,15 @@
 package com.app.nihongo.service.user;
 
 
+import com.app.nihongo.dto.UserExpDTO;
 import com.app.nihongo.dto.UserDTO;
+import com.app.nihongo.dto.UserInfoDTO;
 import com.app.nihongo.entity.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface UserService extends UserDetailsService {
@@ -15,4 +19,10 @@ public interface UserService extends UserDetailsService {
     ResponseEntity<?> saveUser(UserDTO userDTO);
     ResponseEntity<UserDTO> getUserById(int id);
     ResponseEntity<?> updateUser(int id, UserDTO userDTO);
+
+    Integer getUserExperience(Integer userId);
+    List<UserExpDTO> getUserExpByLevel(String level);
+
+
+    ResponseEntity<UserInfoDTO> getUserInfoById(Integer userId);
 }
